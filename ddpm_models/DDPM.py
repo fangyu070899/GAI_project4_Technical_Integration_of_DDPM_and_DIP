02a08d7b87c71d62b48ce561c3b7356bc51113bf
fangyu070899 <96630226+fangyu070899@users.noreply.github.com>
2024-06-18 15:38:49 +0800

    upload models

---
+++ ddpm_models/DDPM.py
@@ -0,0 +1,98 @@
+import random
+import imageio
+import numpy as np
+from argparse import ArgumentParser
+
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
+import time
+import einops
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+from torchvision.transforms import Compose, ToTensor, Lambda, Resize
+from torchvision.datasets.mnist import MNIST, FashionMNIST
+
+# DDPM class
+class MyDDPM(nn.Module):
+    def __init__(self, network, n_steps=200, min_beta=10 ** -4, max_beta=0.02, device=None, image_chw=(1, 32, 32)):
+        super(MyDDPM, self).__init__()
+        self.n_steps = n_steps
+        self.device = device
+        self.image_chw = image_chw
+        self.network = network.to(device)
+        self.betas = torch.linspace(min_beta, max_beta, n_steps).to(
+            device)  # Number of steps is typically in the order of thousands
+        self.alphas = 1 - self.betas
+        self.alpha_bars = torch.tensor([torch.prod(self.alphas[:i + 1]) for i in range(len(self.alphas))]).to(device)
+
+    def forward(self, x0, t, eta=None):
+        # Make input image more noisy (we can directly skip to the desired step)
+        n, c, h, w = x0.shape
+        a_bar = self.alpha_bars[t]
+
+        if eta is None:
+            eta = torch.randn(n, c, h, w).to(self.device)
+
+        noisy = a_bar.sqrt().reshape(n, 1, 1, 1) * x0 + (1 - a_bar).sqrt().reshape(n, 1, 1, 1) * eta
+        return noisy
+
+    def backward(self, x, t):
+        # Run each image through the network for each timestep t in the vector t.
+        # The network returns its estimation of the noise that was added.
+        return self.network(x, t)
+    
+def generate_new_images(ddpm, n_samples=1, device=None, frames_per_gif=100, gif_name="sampling.gif", c=1, h=32, w=32):
+    """Given a DDPM model, a number of samples to be generated and a device, returns some newly generated samples"""
+    frame_idxs = np.linspace(0, ddpm.n_steps, frames_per_gif).astype(np.uint)
+    frames = []
+
+    s = time.time()
+
+    with torch.no_grad():
+        if device is None:
+            device = ddpm.device
+
+        # Starting from random noise
+        x = torch.randn(n_samples, c, h, w).to(device)
+
+        for idx, t in enumerate(list(range(ddpm.n_steps))[::-1]):
+            # Estimating noise to be removed
+            time_tensor = (torch.ones(n_samples, 1) * t).to(device).long()
+            eta_theta = ddpm.backward(x, time_tensor)
+
+            alpha_t = ddpm.alphas[t]
+            alpha_t_bar = ddpm.alpha_bars[t]
+
+            # Partially denoising the image
+            x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_t_bar).sqrt() * eta_theta)
+
+            if t > 0:
+                z = torch.randn(n_samples, c, h, w).to(device)
+
+                # Option 1: sigma_t squared = beta_t
+                beta_t = ddpm.betas[t]
+                sigma_t = beta_t.sqrt()
+
+                # Option 2: sigma_t squared = beta_tilda_t
+                # prev_alpha_t_bar = ddpm.alpha_bars[t-1] if t > 0 else ddpm.alphas[0]
+                # beta_tilda_t = ((1 - prev_alpha_t_bar)/(1 - alpha_t_bar)) * beta_t
+                # sigma_t = beta_tilda_t.sqrt()
+
+                # Adding some more noise like in Langevin Dynamics fashion
+                x = x + sigma_t * z
+
+            # Adding frames to the GIF
+            if idx in frame_idxs or t == 0:
+                # Putting digits in range [0, 255]
+                normalized = x.clone()
+                for i in range(len(normalized)):
+                    normalized[i] -= torch.min(normalized[i])
+                    normalized[i] *= 255 / torch.max(normalized[i])
+
+                    # plt.show()
+                    plt.imshow(np.clip(normalized[i].detach().cpu().numpy()[0],0,255), cmap='gray')
+
+    return x
